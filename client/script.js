0d4bf91c9e99f184ddd912cd7fd51bbf6062ebf5
ahmed-abdat <ahmedeabdat@gmail.com>
2023-01-09 11:31:26 +0000

    last

--- client/script.js
+++ client/script.js
@@ -6,7 +6,8 @@ const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
 let loadInterval
-document.querySelector("textarea").focus()
+const textarea = document.querySelector('textarea')
+textarea.focus()
 function loader(element) {
     element.textContent = ''
 
