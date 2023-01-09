a0b4539dc6c328b2813e277e3344b26ee6253dda
ahmed-abdat <ahmedeabdat@gmail.com>
2023-01-09 12:59:24 +0000

    last

--- client/script.js
+++ client/script.js
@@ -33,7 +33,6 @@ function typeText(element, text) {
             index++
         } else {
             clearInterval(interval)
-            textarea.focus()
             chatContainer.scrollTop = chatContainer.scrollHeight;
         }
     }, 20)
